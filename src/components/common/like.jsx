3e14bc7ee3dbfa0930384ecca744ee0541e6b282
priya <hellopooja0007@gmail.com>
2019-02-09 18:52:21 +0530

    like component

---
+++ src/components/common/like.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Like = props => {
+  let classes = "fa fa-heart";
+  if (!props.liked) {
+    classes += "-o";
+  }
+  return (
+    <i
+      onClick={props.onLike}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="false"
+    />
+  );
+};
+
+export default Like;
---
+++ src/components/movies.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import Like from "./common/like";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movieId => {
+    const filteredMovieArr = this.state.movies.filter(
+      movie => movie._id !== movieId
+    );
+    this.setState({ movies: filteredMovieArr });
+  };
+
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>There are no movies in the database.</p>;
+
+    return (
+      <React.Fragment>
+        <p>Showing {count} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th />
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => {
+              return (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <Like
+                      liked={movie.liked}
+                      onLike={() => this.handleLike(movie)}
+                    />
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      onClick={() => this.handleDelete(movie._id)}
+                      className="btn btn-danger btn-sm"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
